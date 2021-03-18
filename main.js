9819b744faa791d9b7231e6906840475bc7e4cec
Anantjit <anantjitchander@gmail.com>
2021-03-18 20:38:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
+prediction_1="";
+prediction_2="";
+
 Webcam.set({
-    width: 350,
+    width:350,
     height:300,
     image_format:'png',
     png_quality:90
 });
 
 camera=document.getElementById("camera");
+
 Webcam.attach('#camera');
 
 function take_snapshot(){
-    Webcam.snap(function (data_uri){
-        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';
+    webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="captured_image"/>';
     });
 }
 
-console.log('ml5 version',ml5.version);
+console.log('ml5 version', ml5.version);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LmNl35yjS/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/MHEn8aHHO/models.json', modelLoaded);
+
 function modelLoaded(){
-    console.log('modelLoaded!');
+    console.log(modelLoaded);
 }
 
-function check(){
-    img=document.getElementById('captured_image');
-    classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results){ 
-    if (error)
-    {
-        console.error(hello);
-    }
-    else
-    {
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML=results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);    
-    }
+function speak(){
+    var synth=window.SpeachSynthesis;
+    speak_data_1="The first perdiction is"+prediction_1;
+    speak_data_2="The second prediction is"+prediction_2;
+    var utterThis=new SpeachSynthesisIsUtterence(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
 }
